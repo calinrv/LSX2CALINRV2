dc55150dcf5763809b184a269a29a249e00e4770
calinrv91 <calinrv91@yahoo.com>
2016-08-02 19:32:46 -0600

    History Script problema 2 actualizacion 3.0

--- tareas/Proyecto_Programado/src/problema2.sh
+++ tareas/Proyecto_Programado/src/problema2.sh
@@ -29,10 +29,40 @@ for e in 'find $OUT_DATA -name "*.csv"'
 do
         echo "Dando Formato de datos para graficar el archivo $e"
 ###con la expresion de cat damos formato a la salida de datos en donde se ordenan por columnas del 1 al 5 y luego se eimprimen los datos
-cat $e | awk -F "\",\"" '{print $1 " " $2 " " $3}' | sed '1,$ s/"//g' > $GRAF_DATA/graf-$m.dat
+cat $e | awk -F "\",\"" '{print $1 " " $2 " " $3}' > $GRAF_DATA/graf-$m.dat
 
         let m=m+1
 
 done 2> error2.log
 
 
+for k in 'find $GRAF_DATA -name "*.dat"'
+do
+        sed 'Id' $k >> $FULL_DATA/full.dat
+        echo "Procesando archivo $k"
+
+done 2> error3.log
+
+
+
+Servicio_BEGIN='Agua 14498'
+Servicio_END='Cable 10000'
+DATA_DONE=$FULL_DATA/full.dat
+
+####Como se menciona en archivo plot el metodo graficar se encarga de dar formato a la salida de los datos y se imprime mediante la imagen fig1 y se establecen los rangos de inicio y fin
+graficar()
+{
+        gnuplot << EOF 2> error.log 
+   
+        set xdata time
+	set "%Y%m%d"
+	set xrange ["$Servicio_BEGIN" : "$Servicio_END"]
+        set terminal png
+        set output 'fig2.png'
+        plot "$DATA_DONE" using 1:2 with lines title "Monto Total"
+
+EOF
+
+}
+
+graficar
